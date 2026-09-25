145b16406d36b6a23a556b8676b0df32662f737e
Joshitha <joshithaburri@gmail.com>
2026-09-25 23:06:39 +0530

    modified

--- Day 1/print.py
+++ Day 1/print.py
@@ -1,1 +1,12 @@
-print("Hello world")
+a=input("enter a")
+b=input("enter b")
+
+name="joshitha"
+print(name.upper())
+print(5/2)
+print(-5/2)
+print(-5//2)
+print(5//2)
+print(5%2)
+print(-5%2)
+print(3>2)
